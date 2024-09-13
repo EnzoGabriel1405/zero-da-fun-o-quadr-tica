416a1ff8e5cabc57d389ba8957e1ab0ca060b3e2
Enzo Gabriel <159266692+EnzoGabriel1405@users.noreply.github.com>
2024-09-13 06:28:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let delta = (b*b)*(-4*a*c);
-delta = Math.sqrt(delta)
-let x_1 = (-b+delta) /2*0
-let x_2 = (-b-delta) /2*0
+function bask(){
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+    let delta = (b*b)*(-4*a*c);
+    let rdelta = Math.sqrt (delta);
 
-if (delta>0){
-
-}else if (delta<0)
+    if (delta == 0){
+        alert('sem resolução quando delta é igual a 0');
+    }else if (delta < 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById('res').innerText = x1;
+    }else{
+        let x1 = (-b+rdelta)/(2*a);
+        let x2 = (-b-rdelta)/(2*a);
+        document.getElementById('res').innerText = 'Resultado: ' + x1 + ' e ' + x2;
+    }
+}
